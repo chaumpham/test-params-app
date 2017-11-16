4957aaa8118251b3dd31513c0c5762e3b2e64364
Chau Pham <theonlychau@gmail.com>
2017-11-15 20:11:36 -0600

    better url format

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -1,5 +1,5 @@
 class V1::GamesController < ApplicationController
-  def query_games_method
+  def query_names_method
     input_name = params["name"]
     if input_name[0] == "a"
       render json: {
@@ -8,6 +8,40 @@ class V1::GamesController < ApplicationController
     else 
       render json: {
         name_massage: "Hey #{input_name}! It's nice to meet you."
+      }
+    end
+  end
+  def query_games_method
+    input_num = params["guess"].to_i
+    correct_answer = 36
+    if input_num == correct_answer
+      render json: {
+        result: "YOU WIN!"
+      }
+    elsif input_num > correct_answer
+      render json: {
+        result: "TOO HIGH!"
+      }
+    else 
+      render json: {
+        result: "TOO LOW!"
+      }
+    end
+  end
+  def query_games_method2
+    input_num = params["some_input_value"].to_i
+    correct_answer = 36
+    if input_num == correct_answer
+      render json: {
+        result: "YOU WIN!"
+      }
+    elsif input_num > correct_answer
+      render json: {
+        result: "TOO HIGH!"
+      }
+    else 
+      render json: {
+        result: "TOO LOW!"
       }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :v1 do 
-    get "/query_games_url" => "games#query_games_method"
+    get "/query_names_url" => "games#query_names_method"
+    get "/query_games_url/" => "games#query_games_method"
+    get "/query_games_url2/:some_input_value" => "games#query_games_method2"
   end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
